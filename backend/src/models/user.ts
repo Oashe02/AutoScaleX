6bb37e57d7536a90c82410b4e5ef6428158fc4dd
Oashe02 <mehtaoashe2006@gmail.com>
2026-03-28 21:14:33 +0530

    update user model

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,11 +1,13 @@
 import mongoose, { Schema } from "mongoose";
 import {userrole} from "../enums/enum"
 
-const user:Schema = new Schema({
+const userschema:Schema = new Schema({
     name: { type: String, required: true },
-    phone: { type: Number, unique: true },
+    email:{type:String,required:true,unique:true},
+    phone: { type: Number,unique:true },
+    password:{type:String,required:true},
     vehicleNumber: { type: String, required: true },
     role: { type: String,enum:Object.values(userrole),default: "user" },
 }, { timestamps: true })
 
-export default mongoose.model("user", user)
+export default mongoose.model("user", userschema)
