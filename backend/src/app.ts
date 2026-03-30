7582bc130b46533284d2dc3d949f638f8400d643
Oashe02 <mehtaoashe2006@gmail.com>
2026-03-30 20:31:55 +0530

    added auth in

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,21 @@
 import express from "express"
+import cors from "cors"
+import authroutes from "./routes/auth"
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.get("/start",(req,res)=>{
     res.status(200).json({message:"start ho gaya"})
 })
 
+app.use("/api/auth",authroutes)
+
+app.use((err:any,req:any,res:any,next:any)=>{
+    console.error(err.stack)
+    res.status(500).json({error:"error"})
+})
 
 export default app
