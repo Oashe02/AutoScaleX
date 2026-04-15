d0850670e3c3270333af2c64ea32b92dfb706e68
Oashe02 <mehtaoashe2006@gmail.com>
2026-04-15 14:30:00 +0530

    fix auth logic

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -4,9 +4,9 @@ import {userrole} from "../enums/enum"
 const userschema:Schema = new Schema({
     name: { type: String, required: true },
     email:{type:String,required:true,unique:true},
-    phone: { type: Number,unique:true },
+    phone: { type: String,unique:true },
     password:{type:String,required:true},
-    vehicleNumber: { type: String, required: true },
+    vehicleNumber: {type: String, required: true },
     role: { type: String,enum:Object.values(userrole),default: "user" },
 }, { timestamps: true })
 
